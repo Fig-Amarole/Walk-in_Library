295bf14b0311dc2b00635f5c5a4fbf78e6d2dc9c
Jeffrey-Smith <Jeffrey-Smith@users.noreply.github.com>
2015-01-31 19:40:39 -0600

    Added method stubs to DatabaseWhisperer

--- Walk-in_Library/DatabaseWhisperer.cs
+++ Walk-in_Library/DatabaseWhisperer.cs
@@ -11,12 +11,21 @@ namespace Walk_in_Library
         public DatabaseWhisperer() { }
 
         // Add a row to Books table
+        public void AddBook(Book book)
+        {
 
+        }
         
         // Update a row in Books table
+        public void UpdateBook(Book book)
+        {
 
+        }
 
         // Delete a row in Books table
+        public void DeleteBook(Book book)
+        {
 
+        }
     }
 }
